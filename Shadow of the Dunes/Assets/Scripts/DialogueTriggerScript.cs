4a4f13984abbb74e6453fdc9101298df8c5c134e
Nick <nicholas.preis@mymail.champlain.edu>
2021-09-18 21:03:23 -0400

    Worked on dialogue and dialogue trigger scripts.

---
+++ Shadow of the Dunes/Assets/Scripts/DialogueTriggerScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriggerScript : MonoBehaviour
+{
+    public bool canInteract = false;
+    public string NPCName;
+    public string PlayerName;
+    public DialogueScript dialogue;
+    public TextMesh prompt;
+    void Start()
+    {
+        dialogue = GetComponent<DialogueScript>();
+    }
+
+    void Update()
+    {
+        ActivateDialogue();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            canInteract = true;
+            UnityEngine.Debug.Log("Entered Trigger");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            canInteract = false;
+            UnityEngine.Debug.Log("Exited Trigger");
+        }
+    }
+
+    void ActivateDialogue()
+    {
+        prompt.gameObject.SetActive(canInteract);
+        if (canInteract == true && Input.GetKeyDown(KeyCode.G))
+        {
+            canInteract = false;
+            StartCoroutine(dialogue.Type());
+            //dialogue.NextSentence();
+        }
+    }
+}
