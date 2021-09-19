1ef46385d184e9594ac3b7ed22bea9e072eda157
Nick <nicholas.preis@mymail.champlain.edu>
2021-09-18 21:03:23 -0400

    Worked on dialogue and dialogue trigger scripts.

--- Shadow of the Dunes/Assets/Scripts/DialogueScript.cs
+++ Shadow of the Dunes/Assets/Scripts/DialogueScript.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueScript : MonoBehaviour
 {
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,35 @@ public class DialogueScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    public IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
     }
 }
