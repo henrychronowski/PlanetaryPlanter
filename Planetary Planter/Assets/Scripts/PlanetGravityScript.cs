979d33ed4cb2b8ff81024d717afb3d61bf00f18b
Daniel Hartman <daniel.hman123@gmail.com>
2021-10-11 15:37:44 -0400

    Added cylidrical planetary gravity

--- Planetary Planter/Assets/Scripts/PlanetGravityScript.cs
+++ Planetary Planter/Assets/Scripts/PlanetGravityScript.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlanetGravityScript : MonoBehaviour
 {
     public float gravity = -10;
-
-    public void Attract(Transform body)
+    public Vector3 updatedPosForZ;
+    public Transform center;
+    public Vector3 Attract(Transform body)
     {
+        
         Vector3 gravityUp = (body.position - transform.position).normalized;
         Vector3 bodyUp = body.up;
 
@@ -15,5 +17,26 @@ public class PlanetGravityScript : MonoBehaviour
 
         Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
         body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+        return gravityUp;
+    }
+
+    public Vector3 AttractCylinder(Transform body)
+    {
+        updatedPosForZ = new Vector3(center.position.x, center.position.y, body.position.z);
+        Vector3 gravityUp = (body.position - updatedPosForZ).normalized;
+        Vector3 bodyUp = body.up;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+
+        return gravityUp;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(updatedPosForZ, 1.0f);
     }
 }
