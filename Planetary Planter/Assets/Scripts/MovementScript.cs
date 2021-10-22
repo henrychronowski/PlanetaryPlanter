5e237120e860eff760b9c6d805ab816a09804a79
= <daniel.hartman@mymail.champlain.edu>
2021-10-22 19:03:23 -0400

    Messing with planet sizes

--- Planetary Planter/Assets/Scripts/MovementScript.cs
+++ Planetary Planter/Assets/Scripts/MovementScript.cs
@@ -6,7 +6,7 @@ public class MovementScript : MonoBehaviour
 {
     public float moveSpeed = 15.0f;
     private Vector3 moveDir;
-
+    private Rigidbody rgd;
     // Update is called once per frame
     void Update()
     {
