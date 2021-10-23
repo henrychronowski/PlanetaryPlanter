bf0decf0eab38ea26e4ca5d3793573afd33b3c77
= <daniel.hartman@mymail.champlain.edu>
2021-10-22 21:18:07 -0400

    Testing some new planet and movement stuff

--- Planetary Planter/Assets/Scripts/MovementScript.cs
+++ Planetary Planter/Assets/Scripts/MovementScript.cs
@@ -7,14 +7,61 @@ public class MovementScript : MonoBehaviour
     public float moveSpeed = 15.0f;
     private Vector3 moveDir;
     private Rigidbody rgd;
+
+    public Transform groundCheck;
+    public LayerMask ground;
+    public float groundCheckRadius;
+
+    public float jumpPower;
+
+    [SerializeField]
+    bool grounded;
     // Update is called once per frame
+
+    private void Start()
+    {
+        rgd = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if(Physics.CheckSphere(groundCheck.position, groundCheckRadius, ground))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rgd.AddForce(transform.up * jumpPower);
+        }
     }
 
     void FixedUpdate()
     {
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+        //rgd.MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+        rgd.AddForce(transform.TransformDirection(moveDir) * moveSpeed);
+        Vector3.ClampMagnitude(rgd.velocity, 50f);
+        //GetComponent<PlayerGravityScript>().planet.Attract(transform);
+        
+        //transform.LookAt(new Ray(transform.position, moveDir).GetPoint(5f));
+        //transform.RotateAround(transform.position, new Vector3(0, 1, 0));
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up, moveDir), Time.deltaTime * 50);
+    }
+
+    private void OnDrawGizmos()
+    {
+        //rgd.position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime;
+        
+        Ray a = new Ray(transform.position, transform.TransformDirection(moveDir));
+        Gizmos.DrawLine(transform.position, a.GetPoint(50));
     }
 }
