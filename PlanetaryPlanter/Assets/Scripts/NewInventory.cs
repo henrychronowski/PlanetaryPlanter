0751559ecc59ebaf9b446c482dd2bf2e6fe9f1a1
= <daniel.hartman@mymail.champlain.edu>
2022-03-03 17:06:05 -0500

    Trash can space and various fixes to scene and player changes

--- PlanetaryPlanter/Assets/Scripts/NewInventory.cs
+++ PlanetaryPlanter/Assets/Scripts/NewInventory.cs
@@ -141,6 +141,7 @@ public class NewInventory : MonoBehaviour
         }
         else
         {
+            
             if(space.item == null)
             {
                 space.item = selectedItem;
@@ -152,6 +153,17 @@ public class NewInventory : MonoBehaviour
             }
             else
             {
+                if(space.location == SpaceLocation.Trash)
+                {
+                    Destroy(space.item.gameObject);
+                    space.item = selectedItem;
+                    //selectedItem = null;
+                    itemInCursor = false;
+                    space.filled = true;
+                    space.item.transform.parent = space.transform;
+                    space.item.transform.localPosition = Vector3.zero;
+                }
+
                 InventoryItem temp = space.item;
                 space.item = selectedItem;
                 selectedItem = temp;
