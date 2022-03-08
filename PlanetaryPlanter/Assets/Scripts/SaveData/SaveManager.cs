fbf4c842dd65d35c8fa6108ab434279d78742a78
Daniel Hartman <daniel.hman123@gmail.com>
2022-03-08 03:43:29 -0500

    Fixed load game button, everything seems to work

--- PlanetaryPlanter/Assets/Scripts/SaveData/SaveManager.cs
+++ PlanetaryPlanter/Assets/Scripts/SaveData/SaveManager.cs
@@ -1,7 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public struct PlantsData
+{
+    public PlantsData(List<PlantSpot> planters)
+    {
+        planterIds = new int[planters.Count];
+        speciesArray = new PlanetSpecies[planters.Count];
+        growthValue = new float[planters.Count];
+        waterValue = new float[planters.Count];
+
+        for (int i = 0; i < planters.Count; i++)
+        {
+            planterIds[i] = i;
+            if (planters[i].placedPlant == null)
+            {
+                speciesArray[i] = PlanetSpecies.Unidentified;
+                growthValue[i] = -1;
+                waterValue[i] = -1;
+            }
+            else
+            {
+                speciesArray[i] = planters[i].placedPlant.species;
+                growthValue[i] = planters[i].placedPlant.growthProgress;
+                Debug.Log("Save growth as " + growthValue[i]);
+                waterValue[i] = planters[i].placedPlant.currentWater;
+            }
+
+        }
+        
+    }
+
+    public int[] planterIds;
+    public PlanetSpecies[] speciesArray;
+    public float[] growthValue;
+    public float[] waterValue;
+}
+
+[System.Serializable]
+public struct InventoryItemSave
+{
+    public InventoryItemSave(string name, ItemID itemId)
+    {
+        spaceName = name;
+        id = itemId;
+    }
+    public string spaceName;
+    public ItemID id;
+}
 public class SaveManager : MonoBehaviour
 {
     public static SaveManager instance;
@@ -15,7 +65,7 @@ public class SaveManager : MonoBehaviour
     public bool dataLoaded;
     public bool loadDataIntended;
 
-
+    
 
     private void Awake()
     {
@@ -32,7 +82,11 @@ public class SaveManager : MonoBehaviour
 
     public void SaveData()
     {
-        SaveSystem.SaveAllData(player, master, AlmanacProgression.instance, TutorialManagerScript.instance, NewInventory.instance);
+        List<PlantSpot> plantSpots = new List<PlantSpot>();
+        plantSpots.AddRange(GameObject.FindObjectsOfType<PlantSpot>(true).OrderBy(gameObject => gameObject.name).ToArray());
+        master = GameObject.FindObjectOfType<ObservatoryMaster>();
+        player = GameObject.FindObjectOfType<CharacterMovement>();
+        SaveSystem.SaveAllData(player, master, AlmanacProgression.instance, TutorialManagerScript.instance, NewInventory.instance, new PlantsData(plantSpots));
     }
 
     public void LoadData()
@@ -51,6 +105,14 @@ public class SaveManager : MonoBehaviour
         AlmanacProgression.instance.LoadCompletedAchievements(playerData.achievementsDone);
         TutorialManagerScript.instance.LoadCompletedTutorials(playerData.tutorialsDone);
         NewInventory.instance.LoadAllItems(GameObject.FindObjectOfType<InventoryItemIndex>(), playerData.itemIDs);
+
+        //Spawn all the new plants
+        PlantSpot[] planters = GameObject.FindObjectsOfType<PlantSpot>(true).OrderBy(gameObject => gameObject.name).ToArray();
+        for(int i = 0; i < playerData.plants.planterIds.Length; i++)
+        {
+            planters[i].PlacePlantOfType(playerData.plants.speciesArray[i], playerData.plants.growthValue[i]);
+            //planters[i].placedPlant.currentWater = playerData.plants.waterValue[i];
+        }
     }
 
     public void LoadGame()
@@ -72,7 +134,7 @@ public class SaveManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (SceneManager.GetActiveScene().buildIndex == activeSceneIndex)
         {
--- PlanetaryPlanter/Assets/Scripts/SaveData/SaveSystem.cs
+++ PlanetaryPlanter/Assets/Scripts/SaveData/SaveSystem.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
 //Mostly taken from Brackeys vid https://youtu.be/XOjd_qU2Ido
+
 public static class SaveSystem
 {
-    public static void SaveAllData(CharacterMovement player, ObservatoryMaster observatoryMaster, AlmanacProgression alm, TutorialManagerScript tutorials, NewInventory inventory)
+    public static void SaveAllData(CharacterMovement player, ObservatoryMaster observatoryMaster, AlmanacProgression alm, TutorialManagerScript tutorials, NewInventory inventory, PlantsData plants)
     {
         BinaryFormatter formatter = new BinaryFormatter();
         string path = Application.persistentDataPath + "/squimbus.progress";
         FileStream fs = new FileStream(path, FileMode.Create);
 
-        PlayerData data = new PlayerData(player, observatoryMaster, alm, tutorials, inventory);
+        PlayerData data = new PlayerData(player, observatoryMaster, alm, tutorials, inventory, plants);
 
-        formatter.Serialize(fs, data);
+        try
+        {
+            formatter.Serialize(fs, data);
+        }
+        catch (System.Exception)
+        {
+            fs.Close();
+            Debug.LogError("Save failed, exception");
+            throw;
+        }
         fs.Close();
         Debug.Log("Save complete to " + path);
     }
@@ -24,8 +35,18 @@ public static class SaveSystem
         {
             BinaryFormatter formatter = new BinaryFormatter();
             FileStream fs = new FileStream(path, FileMode.Open);
+            PlayerData data;
+            try
+            {
+                data = formatter.Deserialize(fs) as PlayerData;
 
-            PlayerData data = formatter.Deserialize(fs) as PlayerData;
+            }
+            catch (System.Exception)
+            {
+                fs.Close();
+                Debug.LogError("Load failed, exception");
+                throw;
+            }
 
             fs.Close();
             Debug.Log("Load complete from " + path);
