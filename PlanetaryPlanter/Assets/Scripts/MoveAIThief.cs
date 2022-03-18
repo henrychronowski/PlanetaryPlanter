fa647a45e4c20b775ff3159402f470d6f56789fb
Matt <matthew.esslie@mymail.champlain.edu>
2022-03-18 10:39:03 -0400

    making nav mesh for the ai thief

--- PlanetaryPlanter/Assets/Scripts/MoveAIThief.cs
+++ PlanetaryPlanter/Assets/Scripts/MoveAIThief.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveAIThief : MonoBehaviour
 {
+    public float moveSpeed; //how fast the ai will move
+    public NavMeshAgent thief;
+
+    public GameObject player; //to know the player to target
+    public Transform detectionRadius; //how close the player must be to be noticed
+    public Transform movementRadius; //the range on the map the ai can move
+
+    Vector3 destination;
+    bool newDestinationNeeded = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,17 @@ public class MoveAIThief : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //gameObject.GetComponent<NavMeshAgent>().SetDestination();
+
+        CheckThiefLocation();
+    }
+
+    void CheckThiefLocation()
+    {
+        if (gameObject.transform.position == destination)
+        {
+            //pick new location to move to
+            newDestinationNeeded = true;
+        }
     }
 }
