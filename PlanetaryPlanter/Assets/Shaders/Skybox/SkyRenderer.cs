4e8d18f1b5011c309103404292a4f841ca620030
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2022-03-29 14:08:02 -0400

    Adding URP, removing HDRP. HAS SHADER ERRORS

--- PlanetaryPlanter/Assets/Shaders/Skybox/SkyRenderer.cs
+++ PlanetaryPlanter/Assets/Shaders/Skybox/SkyRenderer.cs
@@ -1,59 +1,59 @@
-using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.HighDefinition;
+//using UnityEngine;
+//using UnityEngine.Rendering;
+//using UnityEngine.Rendering.HighDefinition;
 
-class NewSkyRenderer : SkyRenderer
-{
-	public static readonly int _Cubemap = Shader.PropertyToID("_Cubemap");
-	public static readonly int _SkyParam = Shader.PropertyToID("_SkyParam");
-	public static readonly int _PixelCoordToViewDirWS = Shader.PropertyToID("_PixelCoordToViewDirWS");
+//class NewSkyRenderer : SkyRenderer
+//{
+//	public static readonly int _Cubemap = Shader.PropertyToID("_Cubemap");
+//	public static readonly int _SkyParam = Shader.PropertyToID("_SkyParam");
+//	public static readonly int _PixelCoordToViewDirWS = Shader.PropertyToID("_PixelCoordToViewDirWS");
 
-	public static readonly int _SkyColor = Shader.PropertyToID("_SkyColor");
+//	public static readonly int _SkyColor = Shader.PropertyToID("_SkyColor");
 
-	Material m_NewSkyMaterial;
-	MaterialPropertyBlock m_PropertyBlock = new MaterialPropertyBlock();
+//	Material m_NewSkyMaterial;
+//	MaterialPropertyBlock m_PropertyBlock = new MaterialPropertyBlock();
 
-	private static int m_RenderCubemapID = 0;
-	private static int m_RenderFullscreenSkyID = 1;
+//	private static int m_RenderCubemapID = 0;
+//	private static int m_RenderFullscreenSkyID = 1;
 
-	public override void Build()
-	{
-		m_NewSkyMaterial = CoreUtils.CreateEngineMaterial(GetNewSkyShader());
-	}
+//	public override void Build()
+//	{
+//		m_NewSkyMaterial = CoreUtils.CreateEngineMaterial(GetNewSkyShader());
+//	}
 
-	Shader GetNewSkyShader()
-	{
-		return Shader.Find("Hidden/HDRP/Sky/SkyShader");//SkyShader");
-	}
+//	Shader GetNewSkyShader()
+//	{
+//		return Shader.Find("Hidden/HDRP/Sky/SkyShader");//SkyShader");
+//	}
 
-	public override void Cleanup()
-	{
-		CoreUtils.Destroy(m_NewSkyMaterial);
-	}
+//	public override void Cleanup()
+//	{
+//		CoreUtils.Destroy(m_NewSkyMaterial);
+//	}
 
-	protected override bool Update(BuiltinSkyParameters builtinParams)
-	{
-		return base.Update(builtinParams);
-	}
+//	protected override bool Update(BuiltinSkyParameters builtinParams)
+//	{
+//		return base.Update(builtinParams);
+//	}
 
-	public override void RenderSky(BuiltinSkyParameters builtinParams, bool renderForCubemap, bool renderSunDisk)
-	{
-		using (new ProfilingSample(builtinParams.commandBuffer, "Draw sky"))
-		{
-			var newSky = builtinParams.skySettings as NewSky;
+//	public override void RenderSky(BuiltinSkyParameters builtinParams, bool renderForCubemap, bool renderSunDisk)
+//	{
+//		using (new ProfilingSample(builtinParams.commandBuffer, "Draw sky"))
+//		{
+//			var newSky = builtinParams.skySettings as NewSky;
 
-			int passID = renderForCubemap ? m_RenderCubemapID : m_RenderFullscreenSkyID;
+//			int passID = renderForCubemap ? m_RenderCubemapID : m_RenderFullscreenSkyID;
 			
-			float intensity = GetSkyIntensity(newSky, builtinParams.debugSettings);
-			float phi = -Mathf.Deg2Rad * newSky.rotation.value;
-			m_PropertyBlock.SetTexture(_Cubemap, newSky.hdriSky.value);
-			m_PropertyBlock.SetVector(_SkyParam, new Vector4(intensity, 0.0f, Mathf.Cos(phi), Mathf.Sin(phi)));
-			m_PropertyBlock.SetMatrix(_PixelCoordToViewDirWS, builtinParams.pixelCoordToViewDirMatrix);
+//			float intensity = GetSkyIntensity(newSky, builtinParams.debugSettings);
+//			float phi = -Mathf.Deg2Rad * newSky.rotation.value;
+//			m_PropertyBlock.SetTexture(_Cubemap, newSky.hdriSky.value);
+//			m_PropertyBlock.SetVector(_SkyParam, new Vector4(intensity, 0.0f, Mathf.Cos(phi), Mathf.Sin(phi)));
+//			m_PropertyBlock.SetMatrix(_PixelCoordToViewDirWS, builtinParams.pixelCoordToViewDirMatrix);
 
-			//m_PropertyBlock.SetVector(_SkyColor, newSky.hdriParam.value);
-			m_PropertyBlock.SetColor(_SkyColor, newSky.hdriParam.value);
+//			//m_PropertyBlock.SetVector(_SkyColor, newSky.hdriParam.value);
+//			m_PropertyBlock.SetColor(_SkyColor, newSky.hdriParam.value);
 
-			CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_NewSkyMaterial, m_PropertyBlock, passID);
-		}
-	}
-}
+//			CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_NewSkyMaterial, m_PropertyBlock, passID);
+//		}
+//	}
+//}
