3c1a40c7c75c0bf6370811e1d87defb89a22c213
Matt <matthew.esslie@mymail.champlain.edu>
2022-03-30 09:49:23 -0400

    Revert "Merge branch 'feature_ThiefAI' into dev"
    This reverts commit 548ad21c2753a0bf604605d5150a2806bd154e19.

--- PlanetaryPlanter/Assets/Scripts/OpenAlmanacScript.cs
+++ PlanetaryPlanter/Assets/Scripts/OpenAlmanacScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OpenAlmanacScript : MonoBehaviour
 {
     public bool isPaused = false;
+    public GameObject pauseMenu;
     public GameObject almanac;
 
     // Start is called before the first frame update
@@ -24,6 +25,31 @@ public class OpenAlmanacScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Q))
         {
             if(isPaused == false)
+            {
+                NewInventory.instance.SetSpacesActive(true);
+                isPaused = true;
+                Time.timeScale = 0;
+                GameObject.FindObjectOfType<CharacterMovement>().canMove = false;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                NewInventory.instance.SetSpacesActive(false);
+                isPaused = false;
+                Time.timeScale = 1;
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(false);
+                }
+                if (!GameObject.FindObjectOfType<ObservatoryMaster>().inObservatoryView)
+                {
+                    GameObject.FindObjectOfType<CharacterMovement>().canMove = true;
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (isPaused == false)
             {
                 NewInventory.instance.SetSpacesActive(true);
                 isPaused = true;
@@ -50,6 +76,20 @@ public class OpenAlmanacScript : MonoBehaviour
 
     public void PauseGame()
     {
+        NewInventory.instance.SetSpacesActive(true);
+        isPaused = true;
+        Time.timeScale = 0;
+        GameObject.FindObjectOfType<CharacterMovement>().canMove = false;
+        pauseMenu.SetActive(true);
+    }
+
+    public void OpenAlmanac()
+    {
+        if(isPaused && almanac.activeInHierarchy)
+        {
+            UnpauseGame();
+            return;
+        }
         NewInventory.instance.SetSpacesActive(true);
         isPaused = true;
         Time.timeScale = 0;
