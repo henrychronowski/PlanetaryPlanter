ef1741ffa8fb14e22ebc8d2fc8de1ce9ca458aca
Matt <matthew.esslie@mymail.champlain.edu>
2022-03-30 15:27:17 -0400

    refixing all components

--- PlanetaryPlanter/Assets/Scripts/PlayerEscaped.cs
+++ PlanetaryPlanter/Assets/Scripts/PlayerEscaped.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerEscaped : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject thief;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,13 @@ public class PlayerEscaped : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            thief.GetComponent<MoveAIThief>().PlayerEscaped();
+        }
     }
 }
