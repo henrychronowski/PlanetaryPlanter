4456080e469d40b0d6358b4024694ee6d9231845
Matt <matthew.esslie@mymail.champlain.edu>
2022-04-02 17:12:04 -0400

    working on scripts for taking item back from ai

---
+++ PlanetaryPlanter/Assets/Scripts/BonkAIThief.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonkAIThief : MonoBehaviour
+{
+    public GameObject thief;
+    public GameObject inventory;
+
+    GameObject recoveredObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RecoverStolenItem()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            if (Vector3.Distance(gameObject.transform.position, thief.transform.position)
+                < 2.0f)
+            {
+                recoveredObject = thief.GetComponent<MoveAIThief>().GetStolenObject();
+                inventory.GetComponent<NewInventory>().AddItem(recoveredObject);
+            }
+        }
+    }
+}
