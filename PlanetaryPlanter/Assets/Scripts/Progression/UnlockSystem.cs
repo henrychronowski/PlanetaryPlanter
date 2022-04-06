5c7081705f0bc24c8fe69f3a7436050b14b59f34
Daniel Hartman <daniel.hman123@gmail.com>
2022-04-06 18:46:50 -0400

    Portal plants and cutscenes!

--- PlanetaryPlanter/Assets/Scripts/Progression/UnlockSystem.cs
+++ PlanetaryPlanter/Assets/Scripts/Progression/UnlockSystem.cs
@@ -11,6 +11,8 @@ public class UnlockSystem : MonoBehaviour
     public GameObject silo1;
     public GameObject silo2;
     public GameObject bounceblooms;
+
+    public bool seniorShowUnlocks;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,25 +40,15 @@ public class UnlockSystem : MonoBehaviour
 
     void Unlock() //0 = Chapter 1, 1 = Chapter 2, etc
     {
+        if(seniorShowUnlocks)
+        {
+            SeniorShowUnlock();
+            return;
+        }
+
         switch (currentChapter)
         {
             case 1:
-                {
-                    //set all bouncy plants to active
-                    bounceblooms.SetActive(true);
-
-                    TutorialManagerScript.instance.Unlock("Bouncebloom");
-                    break;
-                }
-            case 2:
-                {
-                    GameObject.FindObjectOfType<CharacterMovement>().canGlide = true;
-                    TutorialManagerScript.instance.Unlock("Melmee");
-
-                    //glider
-                    break;
-                }
-            case 3:
                 {
                     //new silo
                     silo1.SetActive(true);
@@ -64,11 +56,25 @@ public class UnlockSystem : MonoBehaviour
 
                     break;
                 }
-            case 4:
+            case 2:
                 {
                     //new planters
                     planterSet1.SetActive(true);
                     TutorialManagerScript.instance.Unlock("New Planters");
+                    break;
+                }
+            case 3:
+                {
+                    //set all bouncy plants to active
+                    bounceblooms.SetActive(true);
+
+                    TutorialManagerScript.instance.Unlock("Bouncebloom");
+                    break;
+                }
+            case 4:
+                {
+                    planterSet2.SetActive(true);
+                    TutorialManagerScript.instance.Unlock("New Planters 2");
                     break;
                 }
             case 5:
@@ -79,15 +85,78 @@ public class UnlockSystem : MonoBehaviour
                 }
             case 6:
                 {
-                    planterSet2.SetActive(true);
-                    TutorialManagerScript.instance.Unlock("New Planters 2");
+                    GameObject.FindObjectOfType<AttackHitbox>().canBreakRocks = true;
+                    TutorialManagerScript.instance.Unlock("Stallanovium Trowel");
+
                     break;
                 }
             case 7:
                 {
+                    GameObject.FindObjectOfType<CharacterMovement>().canGlide = true;
+                    TutorialManagerScript.instance.Unlock("Melmee");
+
+                    //glider
+                    break;
+                }
+            case 8:
+                {
+                    break;
+                }
+            case 9:
+                {
+                    break;
+                }
+        }
+    }
+
+    void SeniorShowUnlock() //0 = Chapter 1, 1 = Chapter 2, etc
+    {
+        switch (currentChapter)
+        {
+            case 1:
+                {
+                    //set all bouncy plants to active
+                    bounceblooms.SetActive(true);
+                    TutorialManagerScript.instance.Unlock("Bounceblooms!");
+                    break;
+                }
+            case 2:
+                {
                     GameObject.FindObjectOfType<AttackHitbox>().canBreakRocks = true;
                     TutorialManagerScript.instance.Unlock("Stallanovium Trowel");
-
+                    break;
+                }
+            case 3:
+                {
+                    //glider
+                    GameObject.FindObjectOfType<CharacterMovement>().canGlide = true;
+                    TutorialManagerScript.instance.Unlock("Melmee!");
+                    break;
+                }
+            case 4:
+                {
+                    //new silo
+                    silo1.SetActive(true);
+                    TutorialManagerScript.instance.Unlock("New Silo");
+                    break;
+                }
+            case 5:
+                {
+                    //new planters
+                    planterSet1.SetActive(true);
+                    TutorialManagerScript.instance.Unlock("New Planters");
+                    break;
+                }
+            case 6:
+                {
+                    silo2.SetActive(true);
+                    TutorialManagerScript.instance.Unlock("New Silo Again");
+                    break;
+                }
+            case 7:
+                {
+                    planterSet2.SetActive(true);
+                    TutorialManagerScript.instance.Unlock("New Planters 2");
                     break;
                 }
             case 8:
