8f005d4eb2cd3074bfb9489c5ad078bbaf50f3d7
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2022-04-19 18:38:37 -0400

    Adding properly located environment transition colliders

--- PlanetaryPlanter/Assets/Scripts/Player/CharacterMovement.cs
+++ PlanetaryPlanter/Assets/Scripts/Player/CharacterMovement.cs
@@ -211,11 +211,11 @@ public class CharacterMovement : MonoBehaviour
         if (collisionDot < 0.1f && !grounded && !holdingGlider && collision.gameObject.tag != "Player" && !grabbingLedge)
         {
             //Draw velocity ray
-            Debug.DrawRay(collision.GetContact(0).point, Vector3.ProjectOnPlane(velocity, collision.GetContact(0).normal), Color.cyan);
+           // Debug.DrawRay(collision.GetContact(0).point, Vector3.ProjectOnPlane(velocity, collision.GetContact(0).normal), Color.cyan);
             currentWall = collision.gameObject;
             //Dot product with the XZ velocity since we don't care about Y velocity here
             Vector3 xzVelocity = new Vector3(velocity.x, 0, velocity.z);
-            Debug.Log(Vector3.Dot(collision.GetContact(0).normal, xzVelocity.normalized));
+            //Debug.Log(Vector3.Dot(collision.GetContact(0).normal, xzVelocity.normalized));
             if(Vector3.Dot(collision.GetContact(0).normal, xzVelocity.normalized) >= wallRunningAngleRequirement && wallNormal != collision.GetContact(0).normal)
             {
                 if(!wallrunning) //Set the rotations only once when entering the wallrun
@@ -232,7 +232,7 @@ public class CharacterMovement : MonoBehaviour
                     }
                     wallrunning = true;
                 }
-                Debug.Log("Wallrun");
+                //Debug.Log("Wallrun");
                 wallNormal = collision.GetContact(0).normal;
             }
         }
@@ -312,7 +312,7 @@ public class CharacterMovement : MonoBehaviour
 
         Ray ray = new Ray(ledgeGrabRayLocation.position, Vector3.down);
         Physics.Raycast(ledgeGrabRayLocation.position, Vector3.down, out RaycastHit hitInfo, ledgeGrabRayLength);
-        Debug.DrawLine(ledgeGrabRayLocation.position, ray.GetPoint(ledgeGrabRayLength), Color.red);
+        //Debug.DrawLine(ledgeGrabRayLocation.position, ray.GetPoint(ledgeGrabRayLength), Color.red);
         if (velocity.y <= 0)
         {
             //Debug.Log("Normal" + hitInfo.normal);
@@ -555,7 +555,7 @@ public class CharacterMovement : MonoBehaviour
         }
         if(velocity.y < -maxFallSpeed && !grounded)
         {
-            Debug.Log("Velocity hit " + velocity.y);
+            //Debug.Log("Velocity hit " + velocity.y);
             velocity.y = -maxFallSpeed;
         }
     }
@@ -738,12 +738,12 @@ public class CharacterMovement : MonoBehaviour
         Physics.Raycast(transform.position, Vector3.down, out slopeHit, characterController.height/2 * slopeForceRayLength);
 
         Ray ray = new Ray(transform.position, Vector3.down);
-        Debug.DrawLine(transform.position, ray.GetPoint(characterController.height / 2 * slopeForceRayLength), Color.green);
+        //Debug.DrawLine(transform.position, ray.GetPoint(characterController.height / 2 * slopeForceRayLength), Color.green);
 
         if(slopeHit.normal != Vector3.up && slopeHit.normal != Vector3.zero && velocity.y <= 0)
         {
             Vector3 force = Vector3.down * characterController.height / 2 * slopeForce;
-            Debug.Log(force);
+            //Debug.Log(force);
             //grounded = true;
             return force;
         }
@@ -761,7 +761,7 @@ public class CharacterMovement : MonoBehaviour
             float slopeFactor = Mathf.Abs(1 - Vector3.Dot(Vector3.up, slopeHit.normal));
             Vector3 slideForce = Vector3.ProjectOnPlane(Vector3.down, slopeHit.normal).normalized * slopeFactor * crouchSlideSpeed;
             //Ray sticks around for 5 seconds
-            Debug.DrawRay(slopeHit.point, slideForce, Color.green, 5f);
+            //Debug.DrawRay(slopeHit.point, slideForce, Color.green, 5f);
 
             return slideForce;
         }
