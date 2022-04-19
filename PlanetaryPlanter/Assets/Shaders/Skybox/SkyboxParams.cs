b5818652c7541e0355989d4530f209a206501dba
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2022-04-19 15:12:36 -0400

    Adding all unformatted data to scriptable object

--- PlanetaryPlanter/Assets/Shaders/Skybox/SkyboxParams.cs
+++ PlanetaryPlanter/Assets/Shaders/Skybox/SkyboxParams.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 [CreateAssetMenu(fileName = "Skybox Parameters", menuName = "ScriptableObjects/Skybox", order =1)]
 public class SkyboxParams : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string SkyName;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Color SkyColor;
+    public Color HorizonColor;
+    public float StarDensity = 4.0f;
+    public float StarSize;
+    public Color SunColor;
+    public float SunSize;
+    public float SunBlend;
+    public Color SunSpotColor;
+    public float SunSpotOpacity;
+    public float WindSpeed;
+    public Texture2D CloudTexture;
+    public float CloudHeight;
+    public float CloudEdge;
+    public Color CloudColor;
+    public float SkyRotationSpeed;
+    public float HorizonOffset;
 }
