6a2a7e683894da4c852d03fb378dcc31c4748fea
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2022-04-23 17:57:06 -0400

    Adding properly rotated and scaled water particles

--- PlanetaryPlanter/Assets/Scripts/Farming/Plant.cs
+++ PlanetaryPlanter/Assets/Scripts/Farming/Plant.cs
@@ -128,14 +128,14 @@ public class Plant : MonoBehaviour
         
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         Vector3 spawnPosition = player.transform.position;
-        //Quaternion spawnRotation = player.transform.rotation;
-        //spawnPoint.LookAt(transform, Vector3.up);
+        Quaternion spawnRotation = player.transform.rotation;
+        spawnRotation.SetLookRotation(2.0f*player.transform.up + player.transform.forward, player.transform.up);
         //spawnRotation.SetLookRotation(transform.position - spawnPosition, Vector3.up);
         Vector3 offset = player.transform.forward * 0.65f;
         offset += player.transform.right * 0.25f;
         offset += player.transform.up * 0.1f;
 
-        Instantiate(waterParticles, spawnPosition + offset, player.transform.rotation);
+        Instantiate(waterParticles, spawnPosition + offset, spawnRotation);
         if(currentWater > maxWater)
         {
             currentWater = maxWater;
