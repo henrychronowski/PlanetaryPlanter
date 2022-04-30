3b6e23906db3875f066f88a24977c501b4049eea
Daniel Hartman <daniel.hman123@gmail.com>
2022-04-30 16:59:27 -0400

    Credits scene implemented, running shovel attack, and bug fixes

--- PlanetaryPlanter/Assets/CreditsMover.cs
+++ PlanetaryPlanter/Assets/CreditsMover.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsMover : MonoBehaviour
 {
     public float speed;
+    public float heightToExit;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +14,20 @@ public class CreditsMover : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+        if(transform.position.y > heightToExit)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
